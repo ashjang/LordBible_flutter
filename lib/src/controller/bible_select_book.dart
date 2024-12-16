576b93edc6887803f8551ef3bc1adceb1d4c151d
Harim <83285319+ashjang@users.noreply.github.com>
2024-12-16 18:08:29 +0900

    [add]: bible_select_book
    - 말씀 구절 택하는 화면에서 book 선택하기

---
+++ lib/src/controller/bible_select_book.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:lord_bible/src/data/bible_data.dart';
+
+class BibleSelectBook extends StatelessWidget {
+  final String? selectedBook;
+  final ValueChanged<String> onBookedSelected;
+
+  const BibleSelectBook({
+    super.key,
+    required this.selectedBook,
+    required this.onBookedSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    double textScaleFactor = MediaQuery.of(context).textScaleFactor;
+    int crossAxisCount = textScaleFactor > 1.0 ? 4 : 5;
+
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: Text("Old Testament",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black54),
+            ),
+          ),
+          buildGridView(context, oldTestament, crossAxisCount),
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: Text("New Testament",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black54),
+            ),
+          ),
+          buildGridView(context, newTestament, crossAxisCount),
+        ],
+      ),
+    );
+  }
+
+  Widget buildGridView(BuildContext context, List<String> books, int crossAxisCount) {
+    double fontSize = MediaQuery.of(context).size.width * 0.04;
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      padding: const EdgeInsets.all(20),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: crossAxisCount,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemCount: books.length,
+      itemBuilder: (context, index) {
+        String book = books[index];
+        return GestureDetector(
+          onTap: () => onBookedSelected(book),
+          child: Container(
+            decoration: BoxDecoration(
+              color: selectedBook == book
+                  ? CupertinoColors.systemFill.withOpacity(0.7)
+                  : CupertinoColors.systemGrey4,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Center(
+              child: Text(
+                book,
+                style: TextStyle(
+                  fontSize: fontSize,
+                  fontWeight: selectedBook == book ? FontWeight.bold : FontWeight.normal,
+                  color: selectedBook == book ? Colors.white : Colors.black,
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
--- lib/src/pages/bible_select.dart
+++ lib/src/pages/bible_select.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lord_bible/src/controller/bible_select_book.dart';
 
 class BibleSelect extends StatefulWidget {
   const BibleSelect({super.key});
@@ -9,10 +10,36 @@ class BibleSelect extends StatefulWidget {
 }
 
 class _BibleSelectState extends State<BibleSelect> {
+  int _selectedSegment = 0;
+  String? address = "선택된 주소";
+  String? selectedBook;
+  int? chapter;
+  int? verse;
+
+  Widget segmentView() {
+    switch (_selectedSegment) {
+      case 0:
+        return BibleSelectBook(
+          selectedBook: selectedBook,
+          onBookedSelected: (book) {
+            setState(() {
+              selectedBook = book;
+              address = "$book";
+              _selectedSegment = 1;
+            });
+          },
+        );
+      case 1:
+        return Center(child: Text('Chapter'),);
+      default:
+        return Center(child: Text('Unknown'),);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
+    return Scaffold(
+      appBar: CupertinoNavigationBar(
           heroTag: 'bible_select_tag',
           transitionBetweenRoutes: false,
           middle: Text("Select Bible Verses", style: TextStyle(fontWeight: FontWeight.bold)),
@@ -30,9 +57,40 @@ class _BibleSelectState extends State<BibleSelect> {
           backgroundColor: Colors.transparent,
           border: Border(bottom: BorderSide(color: Colors.transparent))
       ),
-      child: CustomScrollView(
 
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: CupertinoSlidingSegmentedControl(
+                backgroundColor: CupertinoColors.systemGrey2,
+                thumbColor: Colors.white54,
+
+                children: {
+                  0: Padding(padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Text('Book', style:
+                    TextStyle(fontWeight: FontWeight.bold, color: _selectedSegment == 0 ? Colors.black38 : Colors.white),),
+                  ),
+                  1: Padding(padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Text('Chapter', style:
+                    TextStyle(fontWeight: FontWeight.bold, color: _selectedSegment == 1 ? Colors.black38 : Colors.white),),
+                  ),
+                },
+                groupValue: _selectedSegment,
+                onValueChanged: (int? value) {
+                  setState(() {
+                    _selectedSegment = value!;
+                  });
+                }
+            ),
+          ),
+
+          Text("${address}"),
+          SizedBox(height: 10,),
+          Expanded(child: segmentView())
+        ],
       ),
     );
   }
 }
+
