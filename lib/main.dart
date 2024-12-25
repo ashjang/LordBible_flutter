be1aa8a46d990f2b670081c7b68c6ecf45a26118
Harim <83285319+ashjang@users.noreply.github.com>
2024-12-26 02:31:51 +0900

    [add]: permission notification

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:lord_bible/src/binding/init_binding.dart';
 import 'package:lord_bible/src/controller/favorite_controller.dart';
 import 'package:lord_bible/src/controller/scale_controller.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,6 +16,28 @@ void main() async {
   await EasyLocalization.ensureInitialized();
   final prefs = await SharedPreferences.getInstance();
   final isDarkMode = prefs.getBool('isDarkMode') ?? false;
+
+  // 권한 요청 처리
+  bool isPermissionGranted = await _ensureNotificationPermission();
+
+  if (!isPermissionGranted) {
+    // 권한이 거부된 경우 경고 화면 표시
+    runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.black,
+          body: Center(
+            child: Text(
+              'Notification permissions are required to use this app.',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    );
+    return; // 앱 초기화 중지
+  }
 
   Get.put(FavoriteController());
   Get.lazyPut(()=>TextScaleController());
@@ -32,6 +55,29 @@ void main() async {
       )
     );
   });
+}
+
+Future<bool> _ensureNotificationPermission() async {
+  // 권한 요청 결과를 대기
+  while (true) {
+    var status = await Permission.notification.status;
+
+    // 권한이 부여된 경우 true 반환
+    if (status.isGranted) return true;
+
+    // 권한이 거부된 경우 권한 요청 반복
+    if (status.isDenied) {
+      await Permission.notification.request();
+    }
+
+    // 권한 요청이 "사용자가 다시 묻지 않음"으로 설정된 경우 종료
+    if (status.isPermanentlyDenied) {
+      return false; // 권한 요청 불가
+    }
+
+    // 대기 상태 유지
+    await Future.delayed(const Duration(milliseconds: 500));
+  }
 }
 
 class MyApp extends StatelessWidget {
